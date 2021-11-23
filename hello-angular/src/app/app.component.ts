b0bc270864939d05c1ee45078d82030b5ae4ad70
Adi8723 <71640632+Adi8723@users.noreply.github.com>
2021-11-23 18:51:49 +0100

    added image to slide work show

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hello-angular';
+export class AppComponent implements OnInit {
+  images = ['1.jpg','2.jpg','3.jpg'];
+  currentimage = 0;
+  showimage = true;
 
+  ngOnInit(){
+    this.updateimage();
+  }
 
+  updateimage() {
+    setInterval( ()=>{
+      this.currentimage++;
+      this.currentimage = this.currentimage % this.images.length;
+      this.showimage = false;
 
+      setTimeout(()=>{
+        this.showimage = true;
+      },10)
+    },8000)
+  }
   
 }
